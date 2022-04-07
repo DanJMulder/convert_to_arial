69baba27daaeda0c73198f8c39b18da67f14a790
DanJMulder <64587217+DanJMulder@users.noreply.github.com>
2022-04-07 15:03:50 -0400

    added cleaner workflow for intermediate files, $DATE variable so only have to change that once

--- convert_to_arial.sh
+++ convert_to_arial.sh
@@ -1,11 +1,24 @@
 #!/bin/bash
 
+# set the date each time
+DATE=2022-04-**
+
+# work to convert files in the Downloads folder
 mkdir ~/Downloads/converted
-cp -R Clinic\ Notes/  ~/Downloads/converted
-textutil -convert txt /Downloads/converted/AM/*.docx
-textutil -convert docx -font Arial /Downloads/converted/AM/*.txt
-textutil -convert txt /Downloads/converted/PM/*.docx
-textutil -convert docx -font Arial /Downloads/converted/PM/*.txt
-rm /Downloads/converted/AM/*.txt
-rm /converted/PM/*.txt
-cp -R /Downloads/converted /Clinical/Clinic\ Notes/
+cp -R /Users/danielmulder/OneDrive\ -\ Kingston\ Health\ Sciences\ Centre/Clinical/Clinic\ Notes/$DATE/  ~/Downloads/converted
+
+# convert the docx to txt file then convert back to docx while simultaneously converting font to arial
+textutil -convert txt /Users/danielmulder/Downloads/converted/AM/*.docx
+textutil -convert docx -font Arial /Users/danielmulder/Downloads/converted/AM/*.txt
+textutil -convert txt /Users/danielmulder/Downloads/converted/PM/*.docx
+textutil -convert docx -font Arial /Users/danielmulder/Downloads/converted/PM/*.txt
+
+# remove the intermediate .txt files
+rm /Users/danielmulder/Downloads/converted/AM/*.txt
+rm /Users/danielmulder/Downloads/converted/PM/*.txt
+
+# copy the newly converted .docx files back into onedrive
+cp -R /Users/danielmulder/Downloads/converted /Users/danielmulder/OneDrive\ -\ Kingston\ Health\ Sciences\ Centre/Clinical/Clinic\ Notes/$DATE/
+
+# remove the intermediate folder in the Downloads folder
+rm -r /Users/danielmulder/Downloads/converted/
